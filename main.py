0e6ac6addd1550cec67f326abb00bbf528f5c16e
Divyang Mittal <divyangmittal44@gmail.com>
2018-12-08 13:28:31 +0530

    Add buttons to the restart screen

--- main.py
+++ main.py
@@ -6,12 +6,10 @@ pygame.init()
 
 #setup the window display
 size=(1024, 768)
-FPS=60
 width=size[0]
 height=size[1]
 windowSurface = pygame.display.set_mode((size), 0, 32)
 pygame.display.set_caption('Find the key!')
-clock = pygame.time.Clock()
 
 # set up fonts
 basicFont = pygame.font.SysFont(None, 48)
@@ -293,27 +291,64 @@ def search_key():
 
 def show_end_screen():
     BG=pygame.image.load('Images/jail background.jpg')
-    windowSurface.blit(pygame.transform.scale(BG,(size)),(0,0))
-    backtext = pygame.font.SysFont("liberationserif",40)
-    textSurf, textRect = text_objects("Dare to play again!!", backtext)
-    textRect.center = ( (width/2), (height/2) - 200)
-    windowSurface.blit(textSurf, textRect)
-    textSurf, textRect = text_objects("Press space to play again", backtext)
-    textRect.center = ( (width/2), (height/2) + 200)
-    windowSurface.blit(textSurf, textRect)
-    pygame.display.flip()
-    waiting=True
-    while waiting:
-        clock.tick(FPS)
-        for event in pygame.event.get():
-            if event.type==pygame.KEYUP:
-                if event.key==pygame.K_SPACE:
-                    waiting=False
-		    search_key()
-                if event.key==pygame.K_ESCAPE:
-                    pygame.quit();
-            if event.type==pygame.QUIT:
-                pygame.quit();
+    x= y =0 #to track mouse movement
+    buttonWidth=240
+    buttonHeight=50
+    buttonLeft=width/2-(buttonWidth/2)
+    buttonRight=width/2+(buttonWidth/2)
+    #Retart Button
+    restartRect = pygame.Rect((392,509,240,50))
+    #Quit Button
+    quitRect = pygame.Rect((392,584,240,50))
+    event=pygame.event.poll()
+    restart=False #Restart option
+    
+    while event.type != pygame.QUIT:
+
+        windowSurface.blit(pygame.transform.scale(BG,(size)),(0,0))
+
+        event = pygame.event.poll()
+
+	if event.type == pygame.MOUSEMOTION:
+            x, y = event.pos
+
+    	findkey = pygame.font.SysFont("liberationserif",20)
+    	textSurf1, textRect1 = text_objects("PLAY AGAIN", findkey)
+    	textRect1.center = ( (width/2), (height/2) + 150)
+
+    	howtotext = pygame.font.SysFont("liberationserif",20)
+    	textSurf2, textRect2 = text_objects("QUIT", howtotext)
+    	textRect2.center = ( (width/2), (height/2) + 225)
+
+    	if x>=buttonLeft and x<=buttonRight and y>=height/2+125 and y<=height/2+125+buttonHeight:
+		pygame.draw.rect(windowSurface, GOLD, restartRect)
+
+    	else:
+        	pygame.draw.rect(windowSurface, GREEN, restartRect)
+
+    	if x>=buttonLeft and x<=buttonRight and y>=height/2+200 and y<=height/2+200+buttonHeight:
+        	pygame.draw.rect(windowSurface, GOLD, quitRect)
+
+    	else:
+        	pygame.draw.rect(windowSurface, GREEN, quitRect)
+
+
+    	windowSurface.blit(textSurf1, textRect1)
+    	windowSurface.blit(textSurf2, textRect2)
+    	pygame.display.flip()
+
+    	if restart == False:
+            	if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+                	if restartRect.collidepoint(event.pos):
+                    		restart = True
+
+                	elif quitRect.collidepoint(event.pos):
+                    		pygame.quit();
+
+    	if restart == True:
+		search_key()
+
+
 
 if __name__=='__main__':
     search_key()
